9f7c73bbe8a6cbe661c100b20f1f1d8e4eaec7bc
StupidCrow-yuan <719817649@qq.com>
2022-03-02 18:11:30 +0800

    add premake5

---
+++ Hazel/src/Hazel/hzpch.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <functional>
+#include <algorithm>
+
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#ifdef HZ_PLATFORM_WINDOWS
+	#include <Windows.h>
+#endif
